110e46b876ac679dab5fb90848e771366e95f3e3
Zhijiang TAO <taozhijiang@gmail.com>
2016-11-12 16:06:44 +0800

    修改测试用例

--- test/callable_test.cpp
+++ test/callable_test.cpp
@@ -15,9 +15,23 @@ void echo()
     BOOST_LOG_T(info) << "echo output" << endl;
 }
 
-void echo3(const string msg)
+void echo3(string msg)
 {
     BOOST_LOG_T(info) << "std::bind echo output with: " << msg << endl;
+}
+
+struct callobject {
+    void operator()() {
+        BOOST_LOG_T(info) << "struct callable test" << endl;
+    }
+    void memfunc(string msg){
+        BOOST_LOG_T(info) << "struct callable test2 with: " << msg << endl;
+    }
+};
+
+void echo6(string msg)
+{
+    BOOST_LOG_T(info) << "std::function echo output with: " << msg << endl;
 }
 
 BOOST_AUTO_TEST_CASE(test1)
@@ -33,7 +47,17 @@ BOOST_AUTO_TEST_CASE(test1)
     coroutine c3;
     c3.bind_proc(std::bind(echo3, "tzj"));
 
+    coroutine c4, c5;
+    c4.bind_proc(callobject());
+    callobject obj;
+    c5.bind_proc(std::bind(&callobject::memfunc, &obj, "tzj006"));
+
+    c.bind_proc([] {
+        std::cout << "reuse coroutine object!" << endl;
+    });
+
     scheduler::getInstance().RunTask();
+
 
     return;
 }
